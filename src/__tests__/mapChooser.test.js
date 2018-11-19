37abfa4db68df7c6c122401bac1500176f08f773
emmeiwhite <emmeiwhite@gmail.com>
2018-11-19 12:25:05 +0530

    mapChooser functionality is being tested using TDD

--- src/__tests__/mapChooser.test.js
+++ src/__tests__/mapChooser.test.js
@@ -1,14 +1,17 @@
 // We'll simply fail the test first
 import mapChooser from './../mapChooser'; 
 
-// trying to import the functionality to be tested ! Initially it doesn't exist as we first write our test based on the functinality
+// Trying to import the functionality to be tested ! Initially it doesn't exist as we first write our test based on the functinality
 
 describe("mapChooser",()=>{
-    let expected = "sanatnagar.jpg";
+    it("For sanatnagar name, return sanatnagar.jpg",()=>{
+        let expected = "sanatnagar.jpg";
 
-    let actual = mapChooser("sanatnagar");
-
-    // assert
-
-    expect(actual).toEqual(expected);
+        let actual = mapChooser("sanatnagar");
+    
+        // assert
+    
+        expect(actual).toEqual(expected);
+    })
+  
 })
---
+++ src/mapChooser.js
@@ -0,0 +1,8 @@
+// App Functionality: As the user clicks on the location search button, a name is passed along and in return name.jpg is what it should return
+function mapChooser(){
+    return "sanatnagar.jpg";
+    // Let's make our functionality more generic
+    
+}
+
+export default mapChooser;
